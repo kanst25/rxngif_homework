2d1e34a3733a900ca7134a54049dd9be955fafe3
Kavir Naik <knaik2014@kellogg.northwestern.edu>
2013-11-07 13:52:08 -0600

    Finished update and edit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,6 +19,17 @@ def create
   @p.save
 end
 
+def update
+  @picture_update = Picture.find(params[:id])
+  @picture_update.source = params[:source]
+  @picture_update.caption = params[:caption]
+  @picture_update.save
+end
+
+def edit
+  @picture_edited = Picture.find(params[:id])
+end
+
   def destroy
   	@picture_delete = Picture.find(params[:id])
   	@picture_delete.destroy
